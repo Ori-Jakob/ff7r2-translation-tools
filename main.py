114222105d92ac579c77d4ca521733e23b4158d8
Ori Jakob <ori.jakob@gmail.com>
2025-04-11 16:12:58 -0400

    boot straight into CVS tool

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ def main():
     #options = ["Fix CSV File", "CSV Tools", "Exit"]
     options = ["CSV Tools", "Exit"]
 
-    while(True):
-        match(render.start(options, "Pick an option")):
-            #case 0:
-                # original was intended to fix the translation error from the LLM not conforming to proper CSV format
-                # I am stubbing this as it was only needed to fix the file once, but there are other text related issues with it
-                # fix()
-            case 0:
-                tool()
-            case 1:
-                sys.exit(1)
+    # while(True):
+    #     match(render.start(options, "Pick an option")):
+    #         #case 0:
+    #             # original was intended to fix the translation error from the LLM not conforming to proper CSV format
+    #             # I am stubbing this as it was only needed to fix the file once, but there are other text related issues with it
+    #             # fix()
+    #         case 0:
+    #             tool()
+    #         case 1:
+    #             sys.exit(1)
+    tool()
 
 if __name__ == "__main__":
     main()
